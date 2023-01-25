83925abe698128c46d899aed1dcf70e9fd2c09fa
Nithy1anandan <nithyanandan.m.2016@lmsace.com>
2023-01-25 10:44:22 +0530

    version and readme file update

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 
 defined('MOODLE_INTERNAL') || die;
-$plugin->version   = 2023012400; // This component release level.
+$plugin->version   = 2023012500; // This component release level.
 $plugin->maturity = MATURITY_STABLE; // This version's maturity level.
 $plugin->release = 'v4.1'; // This version's release version.
 $plugin->requires  = 2022041900; // This version's moodle require release.
